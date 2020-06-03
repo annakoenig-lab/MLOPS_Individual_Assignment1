ce95b6c9ad8fd069eec3f351c8b8310111bb2477
annakoenig-lab <59959071+annakoenig-lab@users.noreply.github.com>
2020-06-03 11:29:36 +0200

    changes in file

--- src/Iris_model/ML1.py
+++ src/Iris_model/ML1.py
@@ -13,22 +13,21 @@ from sklearn.model_selection import StratifiedKFold
 
 
 def train_model():
-    # Load Iris dataset from the internet
-    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-    dataset = pd.read_csv(url, names=names)
+	# Load Iris dataset from the internet
+	url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+	names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+	dataset = pd.read_csv(url, names=names)
+
+	# Split-out validation dataset
+	array = dataset.values
+	X = array[:,0:4]
+    	y = array[:,4]
+    	X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
 
 
-    # Split-out validation dataset
-    array = dataset.values
-    X = array[:,0:4]
-    y = array[:,4]
-    X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+    	model = SVC(gamma='auto')
 
+    	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    	cv_result = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
 
-    model = SVC(gamma='auto')
-
-    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-    cv_result = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
-
-    return model, cv_result
+    	return model, cv_result
